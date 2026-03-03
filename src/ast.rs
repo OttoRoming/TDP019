5b7ffa5910cc09453ae0be3ca7a07044c20fcd51
Otto Roming <otro@lysator.liu.se>
2026-03-03 18:48:15 +0100

    fixed clippy lints

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,9 @@
+#[allow(unused)]
 struct Block {
     statements: Vec<Statement>,
 }
 
+#[allow(unused)]
 enum AssignmentOperator {
     Equals,   // =
     Add,      // +=
@@ -13,50 +15,59 @@ enum AssignmentOperator {
     Or,       // |=
 }
 
+#[allow(unused)]
 struct AssignmentStatement {
     assignee: Expression,
     operator: AssignmentOperator,
     expression: Expression,
 }
 
+#[allow(unused)]
 enum UpdateOperator {
     Increment, // ++
     Decrement, // --
 }
 
+#[allow(unused)]
 struct UpdateStatement {
     identifier: String,
     operator: UpdateOperator,
 }
 
+#[allow(unused)]
 struct IfStatement {
     test: Expression,
     block: Block,
 }
 // TODO: how to do the else branches?
 
+#[allow(unused)]
 struct WhileStatement {
     test: Expression,
     block: Block,
 }
 
+#[allow(unused)]
 struct EachStatement {
     left: String,
     right: Expression,
     block: Block,
 }
 
+#[allow(unused)]
 struct VariableDeclarationStatement {
     identifier: String,
     type_specifier: Option<TypeSpecifier>,
     expression: Expression,
 }
 
+#[allow(unused)]
 struct Parameter {
     identifier: String,
     type_specifier: TypeSpecifier,
 }
 
+#[allow(unused)]
 struct FunctionDeclarationStatement {
     identifier: String,
     parameters: Vec<Parameter>,
@@ -64,10 +75,12 @@ struct FunctionDeclarationStatement {
     block: Block,
 }
 
+#[allow(unused)]
 struct ReturnStatement {
     expression: Expression,
 }
 
+#[allow(unused)]
 enum Statement {
     Assignment(AssignmentStatement),
     Update(UpdateStatement),
@@ -80,6 +93,7 @@ enum Statement {
     Expression(Expression),
 }
 
+#[allow(unused)]
 enum BinaryOperator {
     Add,
     Subtract,
@@ -96,36 +110,43 @@ enum BinaryOperator {
     NotEquals,
 }
 
+#[allow(unused)]
 struct BinaryExpression {
     left: Expression,
     operator: BinaryOperator,
     right: Expression,
 }
 
+#[allow(unused)]
 enum UnaryOperator {
     Not,    // !
     Negate, // -
 }
 
+#[allow(unused)]
 struct UnaryExpression {
     operator: UnaryOperator,
     right: Expression,
 }
 
+#[allow(unused)]
 struct IdentifierExpression {
     identifier: String,
 }
 
+#[allow(unused)]
 struct FunctionCallExpression {
     callee: Expression,
     arguments: Vec<Expression>,
 }
 
+#[allow(unused)]
 struct IndexExpression {
     collection: Expression,
     index: Expression,
 }
 
+#[allow(unused)]
 enum Expression {
     Binary(Box<BinaryExpression>),
     Unary(Box<UnaryExpression>),
@@ -133,9 +154,10 @@ enum Expression {
     FunctionCall(Box<FunctionCallExpression>),
     Index(Box<IndexExpression>),
     Literal(LiteralExpression),
-    ParenthesisedExpression(Box<Expression>),
+    Parenthesised(Box<Expression>),
 }
 
+#[allow(unused)]
 enum LiteralExpression {
     String(String),
     Int(i64),
@@ -145,4 +167,12 @@ enum LiteralExpression {
     Null,
 }
 
-enum TypeSpecifier {}
+#[allow(unused)]
+enum TypeSpecifier {
+    Int,
+    Float,
+    String,
+    Bool,
+    List(Box<TypeSpecifier>),
+    Ref(Box<TypeSpecifier>),
+}
