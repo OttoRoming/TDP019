db0cede8523473de9ec0ad0701b5c1b7ef44c63f
Otto Roming <otro@lysator.liu.se>
2026-03-05 20:19:52 +0100

    implemented some basic parsing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,6 +6,9 @@ use crate::{
     util::{Location, Region},
 };
 
+#[cfg(test)]
+mod test;
+
 struct Parser {
     index: usize,
     tokens: Vec<Token>,
@@ -28,11 +31,80 @@ impl Parser {
         self.index += 1;
     }
 
-    fn parse_statement(&mut self) -> Result<(), Error> {
-        Ok(())
+    fn matching(&self, expected: Value) -> bool {
+        self.peek(0).value == expected
     }
 
-    pub fn parse_program(&mut self) -> Result<ast::Statement, Error> {}
+    fn parse_literal(&mut self) -> Result<LiteralExpression, Error> {
+        match &self.peek(0).value {
+            Value::KeywordTrue => {
+                self.advance();
+                Ok(LiteralExpression::Bool(true))
+            }
+            Value::KeywordFalse => {
+                self.advance();
+                Ok(LiteralExpression::Bool(false))
+            }
+            _ => Err(error(
+                self.peek(0).region,
+                format!("expected literal, found {:?}", self.peek(0).value),
+            )),
+        }
+    }
+
+    fn parse_equality(&mut self) -> Result<Expression, Error> {
+        let mut expression = Expression::Literal(self.parse_literal()?);
+
+        while self.matching(Value::EqualsOperator) || self.matching(Value::NotEquals) {
+            let operator = Some(match self.peek(0).value {
+                Value::EqualsOperator => BinaryOperator::Equals,
+                Value::NotEquals => BinaryOperator::NotEquals,
+                _ => unreachable!(),
+            });
+            self.advance();
+
+            let right = Some(Expression::Literal(self.parse_literal()?));
+
+            expression = Expression::Binary(Box::new(BinaryExpression {
+                left: expression,
+                operator: operator.unwrap(),
+                right: right.unwrap(),
+            }));
+        }
+
+        Ok(expression)
+    }
+
+    fn parse_or(&mut self) -> Result<Expression, Error> {
+        let mut expression = self.parse_equality()?;
+
+        while self.matching(Value::Or) {
+            let operator = BinaryOperator::Or;
+            self.advance();
+
+            let right = self.parse_equality()?;
+
+            expression = Expression::Binary(Box::new(BinaryExpression {
+                left: expression,
+                operator,
+                right,
+            }));
+        }
+
+        Ok(expression)
+    }
+
+    fn parse_expression(&mut self) -> Result<Expression, Error> {
+        Ok(self.parse_equality()?)
+    }
+
+    fn parse_statement(&mut self) -> Result<Statement, Error> {
+        Ok(Statement::Expression(self.parse_expression()?))
+    }
+
+    pub fn parse_program(&mut self) -> Result<Statement, Error> {
+        self.parse_statement()
+    }
 
     pub fn new(source: &str) -> Result<Self, Error> {
         let tokens = lex(source)?;
@@ -42,4 +114,9 @@ impl Parser {
             tokens: tokens,
         })
     }
+}
+
+pub fn parse(source: &str) -> Result<Statement, Error> {
+    let mut parser = Parser::new(source)?;
+    parser.parse_program()
 }
