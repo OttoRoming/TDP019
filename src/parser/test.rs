1ec597779808896fdf5d781063b1d7eac4d70106
Otto Roming <otro@lysator.liu.se>
2026-03-08 22:37:02 +0100

    improved testing

--- src/parser/test.rs
+++ src/parser/test.rs
@@ -8,10 +8,10 @@ fn parse_logical_or() {
             BinaryExpression {
                 left: Expression::Literal(LiteralExpression::Bool(true)),
                 operator: BinaryOperator::Equals,
-                right: Expression::Literal(LiteralExpression::Bool(true)),
+                right: Expression::Literal(LiteralExpression::Bool(false)),
             }
         )))],
-        parse("true == true;").unwrap()
+        parse("true == false;").unwrap()
     )
 }
 
@@ -22,10 +22,10 @@ fn parse_logical_and() {
             BinaryExpression {
                 left: Expression::Literal(LiteralExpression::Bool(true)),
                 operator: BinaryOperator::And,
-                right: Expression::Literal(LiteralExpression::Bool(true)),
+                right: Expression::Literal(LiteralExpression::Bool(false)),
             }
         )))],
-        parse("true && true;").unwrap()
+        parse("true && false;").unwrap()
     )
 }
 
@@ -36,10 +36,10 @@ fn parse_logical_equality() {
             BinaryExpression {
                 left: Expression::Literal(LiteralExpression::Bool(true)),
                 operator: BinaryOperator::Equals,
-                right: Expression::Literal(LiteralExpression::Bool(true)),
+                right: Expression::Literal(LiteralExpression::Bool(false)),
             }
         )))],
-        parse("true == true;").unwrap()
+        parse("true == false;").unwrap()
     );
 
     assert_eq!(
@@ -47,9 +47,93 @@ fn parse_logical_equality() {
             BinaryExpression {
                 left: Expression::Literal(LiteralExpression::Bool(true)),
                 operator: BinaryOperator::NotEquals,
-                right: Expression::Literal(LiteralExpression::Bool(true)),
+                right: Expression::Literal(LiteralExpression::Bool(false)),
             }
         )))],
-        parse("true != true;").unwrap()
+        parse("true != false;").unwrap()
+    )
+}
+
+#[test]
+fn parse_additive() {
+    assert_eq!(
+        vec![Statement::Expression(Expression::Binary(Box::new(
+            BinaryExpression {
+                left: Expression::Literal(LiteralExpression::Int(10)),
+                operator: BinaryOperator::Add,
+                right: Expression::Literal(LiteralExpression::Int(11))
+            }
+        )))],
+        parse("10 + 11").unwrap()
+    );
+
+    assert_eq!(
+        vec![Statement::Expression(Expression::Binary(Box::new(
+            BinaryExpression {
+                left: Expression::Literal(LiteralExpression::Int(10)),
+                operator: BinaryOperator::Subtract,
+                right: Expression::Literal(LiteralExpression::Int(11))
+            }
+        )))],
+        parse("10 - 11").unwrap()
+    )
+}
+
+#[test]
+fn parse_multiplicative() {
+    assert_eq!(
+        vec![Statement::Expression(Expression::Binary(Box::new(
+            BinaryExpression {
+                left: Expression::Literal(LiteralExpression::Int(10)),
+                operator: BinaryOperator::Multiply,
+                right: Expression::Literal(LiteralExpression::Int(11))
+            }
+        )))],
+        parse("10 * 11").unwrap()
+    );
+
+    assert_eq!(
+        vec![Statement::Expression(Expression::Binary(Box::new(
+            BinaryExpression {
+                left: Expression::Literal(LiteralExpression::Int(10)),
+                operator: BinaryOperator::Divide,
+                right: Expression::Literal(LiteralExpression::Int(11))
+            }
+        )))],
+        parse("10 / 11").unwrap()
+    );
+
+    assert_eq!(
+        vec![Statement::Expression(Expression::Binary(Box::new(
+            BinaryExpression {
+                left: Expression::Literal(LiteralExpression::Int(10)),
+                operator: BinaryOperator::Modulo,
+                right: Expression::Literal(LiteralExpression::Int(11))
+            }
+        )))],
+        parse("10 % 11").unwrap()
+    )
+}
+
+#[test]
+fn parse_unary() {
+    assert_eq!(
+        vec![Statement::Expression(Expression::Unary(Box::new(
+            UnaryExpression {
+                operator: UnaryOperator::Negate,
+                right: Expression::Literal(LiteralExpression::Int(10))
+            }
+        )))],
+        parse("-10").unwrap()
+    );
+
+    assert_eq!(
+        vec![Statement::Expression(Expression::Unary(Box::new(
+            UnaryExpression {
+                operator: UnaryOperator::Not,
+                right: Expression::Literal(LiteralExpression::Bool(true))
+            }
+        )))],
+        parse("!true").unwrap()
     )
 }
